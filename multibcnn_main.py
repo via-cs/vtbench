124d1daa4d2f39f565eab744c01e2e0c7d93441f
vmadhuuu <mvenkat@ucdavis.edu>
2025-02-07 18:04:52 -0800

    joint training model v1

--- multibcnn_main.py
+++ multibcnn_main.py
@@ -7,7 +7,7 @@ import random
 from pathlib import Path
 import torch.nn as nn
 
-from num.models.multibranchCNN import DeepMultiBranchCNN
+from num.models.multibranchCNN import MultiBranchCNN
 from num.multibranch_train import create_dataloaders, train, test
 from num.data_utils import read_ucr, read_ecg5000, normalize_data, to_torch_tensors
 
@@ -33,7 +33,6 @@ def load_config(config_path='config.yaml'):
     with open(config_path, 'r') as f:
         return yaml.safe_load(f)
 
-
 def main():
     set_seed(42)
     parser = argparse.ArgumentParser(description='Train MultiBranch CNN for Time Series Classification')
@@ -43,8 +42,7 @@ def main():
 
     setup_logging()
     config = load_config()
-    fusion_method = config.get("fusion_method", "concatenation")
-
+    
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     config['train_file'] = args.train_path
@@ -57,15 +55,23 @@ def main():
     else:
         x_train, y_train = read_ucr(config['train_file'])
         x_test, y_test = read_ucr(config['test_file'])
+    
     x_train, x_test = normalize_data(x_train, x_test)
     X_train, y_train, X_test, y_test = to_torch_tensors(x_train, y_train, x_test, y_test)
     
-    dataloaders = create_dataloaders(X_train, y_train, X_test, y_test, config)
-       
-    model = DeepMultiBranchCNN(
-        input_channels=3,
-        num_classes=config['num_classes'],
-        fusion_method = fusion_method
+    dataloaders = create_dataloaders(
+        X_train=X_train,
+        y_train=y_train,
+        X_test=X_test,
+        y_test=y_test,
+        config=config,
+    )
+    
+    model = MultiBranchCNN(
+        input_channels=3, 
+        num_classes=config["num_classes"],
+        num_features=X_train.shape[1] if config['numerical_processing']['method'] != "none" else None,
+        config=config
     ).to(device)
         
     trained_model = train(
@@ -73,13 +79,14 @@ def main():
         dataloaders=dataloaders,
         config=config,
         device=device,
-        train_labels = y_train
+        train_labels=y_train
     )
 
     test_metrics = test(
         model=trained_model,
         dataloaders=dataloaders,
-        device=device
+        device=device,
+        config=config
     )
 
 if __name__ == '__main__':
